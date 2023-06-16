c298032f1452d1f49dc4e5877cfbe19018473c32
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-06-15 17:45:41 -0700

    Update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,6 @@
  */
 int main(void)
 {
-FILE *stderr_stream = stderr;
-fputs("and that piece of art is useful\" - Dora Korpar,
-2015-10-19", stderr_stream);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
 return (1);
 }
