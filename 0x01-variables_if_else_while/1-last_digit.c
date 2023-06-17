4a7be36803dc922b25ddb4eef8c0635d576e7ef2
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-06-16 17:28:51 -0700

    Update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,15 +18,15 @@ printf("Last digit of %d is %d and is ", n, lastDigit);
 
 if (lastDigit > 5)
 {
-printf("greater than 5");
+printf("greater than 5\n");
 }
 else if (lastDigit == 0)
 {
-printf("0");
+printf("0\n");
 }
 else
 {
-printf("less than 6 and not 0");
+printf("less than 6 and not 0\n");
 }
 return (0);
 }
