ac2a2813864cccafae2442e4c664113e80bf5470
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-06-21 10:40:42 -0700

    103 fibonicci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,20 +7,23 @@
  */
 int main(void)
 {
-int limit = 50;
-int fib1 = 1, fib2 = 2, sum, fib_next, i;
+int limit = 4000000;
+int fib1 = 1, fib2 = 2, fib_next;
+int sum = 0;
 
-printf("%d, %d", fib1, fib2);
-
-for (i = 3; i <= limit; i++)
+while (fib1 <= limit)
 {
+if (fib1 % 2 == 0)
+{
+sum += fib1;
+}
+
 fib_next = fib1 + fib2;
-sum += fib_next;
 fib1 = fib2;
 fib2 = fib_next;
 }
 
-printf("%d\n",sum);
+printf("%d\n", sum);
 
 return (0);
 }
