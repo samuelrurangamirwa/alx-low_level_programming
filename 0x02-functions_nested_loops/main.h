c9d83365d0803c018e34a6c2ac5ff5b7a68e3572
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-06-20 18:06:59 -0700

    7-print_last_digit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,6 +10,7 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int n);
+int print_last_digit(int);
 void printString(const char *str);
 void print_alphabet(void);
 void print_alphabet_x10(void);
