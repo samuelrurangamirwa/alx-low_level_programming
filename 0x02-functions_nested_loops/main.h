f31de43d034a09f3f9522d222cc7191a0dc9ca20
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-06-20 18:15:37 -0700

    8-24

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,6 +11,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int n);
 int print_last_digit(int);
+void jack_bauer(void);
 void printString(const char *str);
 void print_alphabet(void);
 void print_alphabet_x10(void);
