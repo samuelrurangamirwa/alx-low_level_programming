9a81787bd5968a7bcfedd9145ff28b32b8274d2f
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-06-20 18:23:26 -0700

    9 times table

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,6 +12,7 @@ int print_sign(int n);
 int _abs(int n);
 int print_last_digit(int);
 void jack_bauer(void);
+void times_table(void);
 void printString(const char *str);
 void print_alphabet(void);
 void print_alphabet_x10(void);
