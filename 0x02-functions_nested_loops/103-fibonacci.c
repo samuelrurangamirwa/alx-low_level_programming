26c8bcf2d6e815a12cb04ec3341c35af0271315b
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-06-21 10:20:20 -0700

    103 fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,23 +5,37 @@
  *
  * Return: Always 0 (Success)
  */
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int limit = 50;
-int fib1 = 1, fib2 = 2, fib_next, i;
-
-printf("%d, %d", fib1, fib2);
+int fib1 = 1, fib2 = 2, fib_next,i;
+int sum = fib1 + fib2;
 
 for (i = 3; i <= limit; i++)
 {
 fib_next = fib1 + fib2;
-printf(", %d", fib_next);
+
+sum += fib_next;
 
 fib1 = fib2;
 fib2 = fib_next;
 }
 
-printf("\n");
+printf("%d\n", sum);
 
 return (0);
 }
