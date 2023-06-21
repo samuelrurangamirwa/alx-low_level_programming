2aef0c55d660723819cb988a99f49a1f836afc5f
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-06-21 16:47:57 -0700

    Add 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,14 +8,14 @@
 int main(void)
 {
 int limit = 98, i;
-unsigned int fib1 = 1, fib2 = 2, fib_next;
+int fib1 = 1, fib2 = 2;
 
-printf("%u, %u", fib1, fib2);
+printf("%d, %d", fib1, fib2);
 
 for (i = 3; i <= limit; i++)
 {
-fib_next = fib1 + fib2;
-printf(", %u", fib_next);
+int fib_next = fib1 + fib2;
+printf(", %d", fib_next);
 
 fib1 = fib2;
 fib2 = fib_next;
