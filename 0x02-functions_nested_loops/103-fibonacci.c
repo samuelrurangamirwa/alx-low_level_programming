3103e1840cdfcf22b37599bd5e67dcc4730e51c2
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-06-21 10:09:34 -0700

    103 fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int limit = 50;
+int fib1 = 1, fib2 = 2, fib_next, i;
+
+printf("%d, %d", fib1, fib2);
+
+for (i = 3; i <= limit; i++)
+{
+fib_next = fib1 + fib2;
+printf(", %d", fib_next);
+
+fib1 = fib2;
+fib2 = fib_next;
+}
+
+printf("\n");
+
+return (0);
+}
