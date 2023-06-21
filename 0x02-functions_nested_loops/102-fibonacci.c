219bd19cf8576265fefabb506fc15a6fdd53a125
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-06-21 15:40:52 -0700

    102 fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-int limit = 50;
+int limit = 50,i;
 unsigned long fib1 = 1, fib2 = 2, fib_next;
 
 printf("%lu, %lu", fib1, fib2);
 
-for (int i = 3; i <= limit; i++)
+for (i = 3; i <= limit; i++)
 {
 fib_next = fib1 + fib2;
 printf(", %lu", fib_next);
