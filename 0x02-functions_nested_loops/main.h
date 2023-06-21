73aa1f5200e54c555c9d9665f0e97228a8ac6516
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-06-20 18:37:35 -0700

    11-print_to_98

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,6 +14,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
+void print_to_98(int n);
 void printString(const char *str);
 void print_alphabet(void);
 void print_alphabet_x10(void);
