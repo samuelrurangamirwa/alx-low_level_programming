e85f85ccdb4e404400c9130b051686ea278c86d2
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-06-22 17:09:25 -0700

    Solcing comment issue

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,11 +2,18 @@
 #define MAIN_H
 
 /**
- * printString - Prints a string using _putchar
- * @str: The string to be printed
+ * _isupper - Checks if a character is an uppercase letter
+ * @c: The character to be checked
+ *
+ * Return: 1 if c is an uppercase letter, 0 otherwise
  */
-
 int _isupper(int c);
+/**
+ * _isdigit - Checks if a character is a digit
+ * @c: The character to be checked
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
 int _isdigit(int c);
 
 #endif /* MAIN_H */
