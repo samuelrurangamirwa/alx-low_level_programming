d78d729e4d2f54727dfcbe7d82a0c9a75e5a74ae
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-06-23 13:23:56 -0700

    Debug infinit loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
-
 /**
-* main - causes an infinite loop
-* Return: 0
+* main - Entry point of the program
+*
+* Description: This function prints a message indicating the presence of
+*              an infinite loop and another message for loop avoidance.
+*
+* Return: Always 0 (Success)
 */
-
 int main(void)
 {
-/* int i; */
-
 printf("Infinite loop incoming :(\n");
 
-/* i = 0; */
-/**
+/*
+* Commented out code block to prevent an infinite loop
+*/
+/*
+* int i = 0;
 * while (i < 10)
 * {
 * putchar(i);
+* i++;
 * }
 */
 
+/*
+* Print message to indicate successful infinite loop avoidance
+* Description: This  avoidance of an infinite loop.
+*/
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
