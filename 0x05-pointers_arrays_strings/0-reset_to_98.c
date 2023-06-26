aafe22a0fc9b8eba63a4d64b1890adbffbfad6f3
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-06-26 15:21:06 -0700

    Solving brace '{' and add comment

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,3 +1,8 @@
-void reset_to_98(int *n) {
+/**
+ * reset_to_98 - Updates the value pointed to by the parameter to 98.
+ * @n: Pointer to an int to be updated.
+ */
+void reset_to_98(int *n)
+{
 *n = 98;
 }
