4f1485fa5b49ceb014465e1dee1cf37ec9a66e62
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-06-27 01:47:52 -0700

    Print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* print_array - Prints the elements of an array separated by commas and spaces.
+* @a: The array to be printed.
+* @n: The number of elements in the array.
+*/
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+
+if (i != n - 1)
+{
+printf(", ");
+}
+}
+
+printf("\n");
+}
