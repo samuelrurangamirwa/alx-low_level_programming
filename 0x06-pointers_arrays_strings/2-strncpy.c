066143f6e0dcf490b285196dce627aaa033be3e4
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-06-29 13:03:19 -0700

    Copying string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+/**
+* _strncpy - Copies a string, up to a maximum of n characters.
+* @dest: Pointer to the destination string.
+* @src: Pointer to the source string.
+* @n: Maximum number of characters to copy.
+*
+* Return: Pointer to the resulting string (dest).
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+
+for (; i < n; i++)
+{
+dest[i] = '\0';
+}
+
+return (dest);
+}
