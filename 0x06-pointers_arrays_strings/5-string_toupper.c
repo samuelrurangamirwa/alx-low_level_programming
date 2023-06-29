b4574966a72eacee647dbd3fe6a7a5cce6688eee
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-06-29 13:26:00 -0700

    String to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+/**
+* string_toupper - Converts all lowercase letters of a string to uppercase.
+* @str: Pointer to the string.
+*
+* Return: Pointer to the resulting string.
+*/
+char *string_toupper(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+}
+i++;
+}
+
+return (str);
+}
