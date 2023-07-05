aa55250e2eca39b7397e3aaafcfe1db5e1a14729
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-07-05 09:01:58 -0700

    Add description on function _memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,3 +1,12 @@
+/**
+ * _memset - Fills the first n bytes of the memory area pointed to by s with
+ *           the constant byte b
+ * @s: Pointer to the memory area
+ * @b: Constant byte to fill the memory with
+ * @n: Number of bytes to fill
+ *
+ * Return: Pointer to the memory area s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
