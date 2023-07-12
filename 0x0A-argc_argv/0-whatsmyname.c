39453740e9b234d5ad8a240c7ef89f6fa4106eac
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-07-12 11:36:31 -0700

    Add requirements

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,9 @@
 *
 * Return: Always 0.
 */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 printf("%s\n", argv[0]);
 exit(EXIT_SUCCESS);
-return (argc);
+return (0);
 }
