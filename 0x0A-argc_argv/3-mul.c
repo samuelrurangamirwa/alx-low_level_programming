567111a98bfc76bd34909044b1b12a86623b9798
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-07-13 17:50:15 -0700

    Add Multiples of two number

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 /**
 * main - multiplies two numbers.
 * @argc: argument count
@@ -12,15 +13,19 @@ int main(int argc, char **argv)
 {
 int x, y;
 
+
 if (argc < 3)
 {
 printf("Error\n");
 return (1);
 }
 
+
 x = atoi(argv[1]);
 y = atoi(argv[2]);
-printf("%d\n", x * y);
+printf("%d\n", x * (y));
+
 
 return (0);
 }
+
