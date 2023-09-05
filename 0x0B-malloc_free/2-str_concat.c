a180d3e94e16e0296ce4e5b7e18b7a550c0edc81
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-09-05 15:31:02 -0700

    Function that concatenates two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+
+/**
+ * str_concat - Concatenates two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: A pointer to the concatenated string, or NULL on failure.
+ */
+char *str_concat(char *s1, char *s2)
+{
+char *concat;
+unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
+
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL){
+s2 = "";
+}
+while (s1[len1] != '\0')
+{
+len1++;
+}
+while (s2[len2] != '\0')
+{
+len2++;
+}
+concat = malloc((len1 + len2 + 1) * sizeof(char));
+
+if (concat == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < len1; i++)
+{
+concat[i] = s1[i];
+}
+for (j = 0; j < len2; j++)
+{
+concat[i + j] = s2[j];
+}
+concat[i + j] = '\0';
+
+return (concat);
+}
+
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,6 +4,7 @@
 char *create_array(unsigned int size, char c);
 void simple_print_buffer(char *buffer, unsigned int size);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 
 #define MAX_BUFFER_SIZE 100
 #endif
