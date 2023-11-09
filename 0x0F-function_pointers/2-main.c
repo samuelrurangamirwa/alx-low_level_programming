e50e1c22f87e98eeccb23ce93dafd442ec2037a2
samuelrurangamirwa <rurangamirwa@gmail.com>
2023-11-08 18:39:53 -0800

    Solve character issues

--- 0x0F-function_pointers/2-main.c
+++ 0x0F-function_pointers/2-main.c
@@ -23,7 +23,6 @@ int is_strictly_positive(int elem)
 return (elem > 0);
 }
 
-
 /**
 * abs_is_98 - check if the absolute value of a number is 98
 * @elem: the integer to check
@@ -42,7 +41,10 @@ return (elem == 98 || -elem == 98);
 */
 int main(void)
 {
-int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
+int array[20] = {
+0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2,
+3, 4, 5, 6, 7, 8, 9, 10, 11, 98
+};
 int index;
 
 index = int_index(array, 20, is_98);
@@ -51,5 +53,6 @@ index = int_index(array, 20, abs_is_98);
 printf("%d\n", index);
 index = int_index(array, 20, is_strictly_positive);
 printf("%d\n", index);
+
 return (0);
 }
